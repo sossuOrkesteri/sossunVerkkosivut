9f9148bbaecafd6e75fb97a9d2aadcbbd7751fd2
Reetta S <reetta.savaloja@helsinki.fi>
2021-05-23 12:53:22 +0300

    kansiot

--- app.py
+++ app.py
@@ -4,3 +4,20 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Moi!"
+
+#tilaaSossuKeikalle
+@app.route("/tilaasossu")
+def tilaa():
+	return "Moimoi!"
+
+#mitenSoittajaksi
+@app.route("/mukaan")
+def mukaan():
+	return "Tervetuloa"
+
+#ajankohtaista
+@app.route("/ajankohtaista")
+def ajankohtaista():
+	return "Somet"
+
+
