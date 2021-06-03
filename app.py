ebad6c1e303afd2d416169dff5d42afff1209773
Reetta S <reetta.savaloja@helsinki.fi>
2021-06-03 21:52:26 +0300

    style.css luotu ja linkitetty etusivuun

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
+from flask import render_template
 
 @app.route("/")
 def index():
-    return "Moi!"
+    return render_template("index.html")
 
 #tilaaSossuKeikalle
 @app.route("/tilaasossu")
