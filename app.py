4f039ce984b99fd3ac72edd26ef7504572110164
Reetta S <reetta.savaloja@helsinki.fi>
2021-08-05 12:40:51 +0300

    english side done

--- app.py
+++ app.py
@@ -3,19 +3,32 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/eng")
+def eng_index():
+    return render_template("englishMain.html")
 
 #tilaaSossuKeikalle
 @app.route("/tilaasossu")
 def tilaa():
 	return render_template("tilaasossu.html")
 
+@app.route("/order")
+def order():
+    return render_template("order.html")
+
 #mitenSoittajaksi
 @app.route("/mukaan")
 def mukaan():
 	return render_template("tulemukaan.html")
+
+@app.route("/joinsossu")
+def join():
+    return render_template("joinsossu.html")
 
 #ajankohtaista
 @app.route("/ajankohtaista")
